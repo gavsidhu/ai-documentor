71785159550dd0c79b271da2d18932dd66a90162
gavsidhu <gavinjeet.sidhu@gmail.com>
2023-03-03 23:07:27 -0800

    deleted old front end and renamed new one

--- frontend/src/pages/api/get-session.ts
+++
@@ -1,6 +0,0 @@
-import { NextApiHandler } from 'next';
-import { createServerSupabaseClient } from '@supabase/auth-helpers-nextjs';
-
-const GetSession: NextApiHandler = async (req, res) => {};
-
-export default GetSession;
