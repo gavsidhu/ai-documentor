69afb63eb0cf865d68cb8a06ea1c1754b7770452
Gavin Sidhu <gavinjeet.sidhu@gmail.com>
2023-03-04 22:18:06 -0800

    working on ui

--- optibot-frontend/components/home/Hero.tsx
+++ optibot-frontend/components/home/Hero.tsx
@@ -2,54 +2,27 @@ import React from 'react'
 
 const Hero = () => {
     return (
-        <div className="isolate bg-gray-900">
+        <div>
             <div className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]">
-                <svg
-                    className="relative left-[calc(50%-11rem)] -z-10 h-[21.1875rem] max-w-none -translate-x-1/2 rotate-[30deg] sm:left-[calc(50%-30rem)] sm:h-[42.375rem]"
-                    viewBox="0 0 1155 678"
-                >
-                    <path
-                        fill="url(#f4773080-2a16-4ab4-9fd7-579fec69a4f7)"
-                        fillOpacity=".2"
-                        d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
-                    />
-                    <defs>
-                        <linearGradient
-                            id="f4773080-2a16-4ab4-9fd7-579fec69a4f7"
-                            x1="1155.49"
-                            x2="-78.208"
-                            y1=".177"
-                            y2="474.645"
-                            gradientUnits="userSpaceOnUse"
-                        >
-                            <stop stopColor="#9089FC" />
-                            <stop offset={1} stopColor="#FF80B5" />
-                        </linearGradient>
-                    </defs>
-                </svg>
             </div>
             <div className="px-6 pt-6 lg:px-8">
             </div>
             <main>
                 <div className="relative py-24 sm:py-32 lg:pb-40">
                     <div className="mx-auto max-w-7xl px-6 lg:px-8">
-                        <div className="mx-auto max-w-2xl text-center">
+                        <div className="mx-auto max-w-4xl text-center">
                             <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
-                                Data to enrich your online business
+                            Supercharge Your Development with Optibot
                             </h1>
                             <p className="mt-6 text-lg leading-8 text-gray-300">
-                                Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
-                                fugiat veniam occaecat fugiat aliqua.
+                            Our extension streamlines your workflow and boosts productivity, saving you time and energy. With powerful features and intuitive design, it's the ultimate tool for developers.
                             </p>
-                            <div className="mt-10 flex items-center justify-center gap-x-6">
+                            <div className="mt-10">
                                 <a
                                     href="#"
                                     className="rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400"
                                 >
                                     Get started
-                                </a>
-                                <a href="#" className="text-sm font-semibold leading-6 text-white">
-                                    Learn more <span aria-hidden="true">→</span>
                                 </a>
                             </div>
                         </div>
--- optibot-frontend/components/home/features/Features.tsx
+++ optibot-frontend/components/home/features/Features.tsx
@@ -5,6 +5,16 @@ import styles from './Features.module.css'
 const Features = () => {
   return (
     <div className='max-w-7xl mx-auto'>
+      <div className="mx-auto max-w-2xl py-16 lg:text-center">
+          <h2 className="text-base font-semibold leading-7 text-indigo-600">Deploy faster</h2>
+          <p className="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">
+            Everything you need to deploy your app
+          </p>
+          <p className="mt-6 text-lg leading-8 text-white">
+            Quis tellus eget adipiscing convallis sit sit eget aliquet quis. Suspendisse eget egestas a elementum
+            pulvinar et feugiat blandit at. In mi viverra elit nunc.
+          </p>
+        </div>
       <div className={styles.gridContainer}>
         <div className={styles.item1}>
           <FeatureCard />
