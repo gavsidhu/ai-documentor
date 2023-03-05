d3154153f51f4e4ff5bc03b9ba186f936f378c5c
gavsidhu <gavinjeet.sidhu@gmail.com>
2023-03-04 21:25:57 -0800

    homepage tweaks and adding api routes

---
+++ optibot-frontend/constant/products.ts
@@ -0,0 +1,4 @@
+export const productIds = {
+  standard: 'prod_NSzyi7HH7kAAF4',
+  oneTime: 'prod_NT0DFcVRMuAJau'
+};
---
+++ optibot-frontend/pages/api/optibot/document.ts
@@ -0,0 +1,5 @@
+import { NextApiHandler } from 'next';
+
+const Document: NextApiHandler = async (req, res) => {};
+
+export default Document;
