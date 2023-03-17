1057a2497f40131b37983e7ee5e013e47a49bde4
gavsidhu <gavinjeet.sidhu@gmail.com>
2023-03-17 12:35:42 -0700

    changed which product ids are used based on NODE_ENV

--- optibot-frontend/constant/products.ts
+++ optibot-frontend/constant/products.ts
@@ -1,4 +1,10 @@
-export const productIds = {
-  standard: 'prod_NX2inao2U2IajE',
-  oneTime: 'prod_NX2hbIfYg6aJpr'
-};
+export const productIds =
+  process.env.NODE_ENV == 'development'
+    ? {
+        standard: 'prod_NSzyi7HH7kAAF4',
+        oneTime: 'prod_NT0DFcVRMuAJau'
+      }
+    : {
+        standard: 'prod_NX2inao2U2IajE',
+        oneTime: 'prod_NX2hbIfYg6aJpr'
+      };
